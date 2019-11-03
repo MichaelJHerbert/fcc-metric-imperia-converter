6f7e4afbcf205187bec61a9816ef4633b6a30a5e
Michael <52139933+MichaelJHerbert@users.noreply.github.com>
2019-11-03 18:37:45 +0000

    console logs removed, code tidied up

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,85 +7,75 @@
 */
 
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
+
+  this.getNum = function (input) {
     // Find unit
     var unitRegex = /[a-z]+$/i;
     var unit = input.match(unitRegex);
-    
+
     // Check to see if only numeric value has been entered
-    if(unit === null){
-      console.log('No Unit');
-      if(isNaN(input)){
+    if (unit === null) {
+      if (isNaN(input)) {
         return 'Invalid Number';
       } else {
         return 'No Unit';
       }
     }
-    
+
     var firstCharIndex = input.indexOf(unit[0]);
-    
+
     // Check to see if a number has been entered
-    if(firstCharIndex < 1){
+    if (firstCharIndex < 1) {
       // No number entered so return 1 as default value
-      console.log(1);
       return 1;
     }
-    
+
     // Get number string
     var numString = input.slice(0, firstCharIndex);
-    
+
     // Check to see if fraction has been entered
     var divisionRegex = /\//;
     var divisionRequired = divisionRegex.test(numString);
-    // console.log(divisionRequired)
 
-    if(divisionRequired){
+    if (divisionRequired) {
       var splitString = numString.split('/');
-      
+
       // Check for NaN fractions or too many fractions
-      if(isNaN(splitString[0]) || isNaN(splitString[1]) || splitString.length > 2) {
-        console.log('Invalid Number');
+      if (isNaN(splitString[0]) || isNaN(splitString[1]) || splitString.length > 2) {
         return 'Invalid Number';
       } else {
         // Carry out division
         let result = parseFloat(splitString[0]) / parseFloat(splitString[1]);
-        console.log(result);
         return result;
       }
-    } else if(isNaN(numString)){
+    } else if (isNaN(numString)) {
       // Invalid number entered
-      console.log('Invalid Number');
       return 'Invalid Number';
     } else {
       // No division required & valid number entered
       let result = parseFloat(numString);
-      console.log(result);
       return result;
     }
   };
-  
-  this.getUnit = function(input) {
-    var allowableUnits = ['gal','l','mi','km','lbs','kg'];
+
+  this.getUnit = function (input) {
+    var allowableUnits = ['gal', 'l', 'mi', 'km', 'lbs', 'kg'];
     var unitRegex = /[a-zA-Z]+$/;
     var unit = input.match(unitRegex);
-    
+
     // Check to see if no unit has been entered
-    if(unit === null){
-      console.log('No Unit');
+    if (unit === null) {
       return 'Invalid Unit';
     }
     var lowerCaseUnit = unit[0].toLowerCase();
-    if(allowableUnits.includes(lowerCaseUnit)){
-      console.log(lowerCaseUnit);
+    if (allowableUnits.includes(lowerCaseUnit)) {
       return lowerCaseUnit;
     } else {
-      console.log('Invalid Unit');
       return 'Invalid Unit';
     }
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
     var unitPairs = {
       gal: 'l',
       l: 'gal',
@@ -94,13 +84,12 @@ function ConvertHandler() {
       lbs: 'kg',
       kg: 'lbs'
     };
-    
+
     var result = unitPairs[initUnit];
-    console.log(result);    
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     var unitPairs = {
       gal: 'gallons',
       l: 'litres',
@@ -109,50 +98,42 @@ function ConvertHandler() {
       lbs: 'pounds',
       kg: 'kilograms'
     };
-    
+
     var result = unitPairs[unit];
-    console.log(result);    
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     switch (initUnit) {
       case 'gal':
-        console.log(initNum * galToL);
         return parseFloat((initNum * galToL).toFixed(5));
         break;
       case 'l':
-        console.log(initNum * 1/galToL);
-        return parseFloat((initNum * 1/galToL).toFixed(5));
+        return parseFloat((initNum * 1 / galToL).toFixed(5));
         break;
       case 'lbs':
-        console.log(initNum * lbsToKg);
         return parseFloat((initNum * lbsToKg).toFixed(5));
         break;
       case 'kg':
-        console.log(initNum * 1/lbsToKg);
-        return parseFloat((initNum * 1/lbsToKg).toFixed(5));
+        return parseFloat((initNum * 1 / lbsToKg).toFixed(5));
         break;
       case 'mi':
-        console.log(initNum * miToKm);
         return parseFloat((initNum * miToKm).toFixed(5));
         break;
       case 'km':
-        console.log(initNum * 1/miToKm);
-        return parseFloat((initNum * 1/miToKm).toFixed(5));
+        return parseFloat((initNum * 1 / miToKm).toFixed(5));
         break;
     }
   };
-  
-  this.getString = function(initNum, fullInitUnit, returnNum, fullReturnUnit) {
+
+  this.getString = function (initNum, fullInitUnit, returnNum, fullReturnUnit) {
     var result = `${initNum} ${fullInitUnit} converts to ${returnNum} ${fullReturnUnit}`;
-    console.log(result);
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -12,29 +12,29 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
-    .get(function (req, res){
+    .get(function (req, res) {
       var input = req.query.input; //3.1km
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
-    
-      if(initNum === 'Invalid Number' && initUnit === 'Invalid Unit'){
-        res.json({error: 'Invalid Number and Unit'});
-      } else if(initNum === 'Invalid Number' && initUnit !== 'Invalid Unit') {
-        res.json({error: 'Invalid Number'});
-      } else if(initUnit === 'Invalid Unit' && initNum !== 'Invalid Number'){
-        res.json({error: 'Invalid Unit'})
-      } else{
+
+      if (initNum === 'Invalid Number' && initUnit === 'Invalid Unit') {
+        res.json({ error: 'Invalid Number and Unit' });
+      } else if (initNum === 'Invalid Number' && initUnit !== 'Invalid Unit') {
+        res.json({ error: 'Invalid Number' });
+      } else if (initUnit === 'Invalid Unit' && initNum !== 'Invalid Number') {
+        res.json({ error: 'Invalid Unit' })
+      } else {
         var returnNum = convertHandler.convert(initNum, initUnit);
         var returnUnit = convertHandler.getReturnUnit(initUnit);
         var fullInitUnit = convertHandler.spellOutUnit(initUnit);
         var fullReturnUnit = convertHandler.spellOutUnit(returnUnit);
         var toString = convertHandler.getString(initNum, fullInitUnit, returnNum, fullReturnUnit);
-        res.json({initNum, initUnit, returnNum, returnUnit, string: toString})
+        res.json({ initNum, initUnit, returnNum, returnUnit, string: toString })
       }
     });
-    
+
 };
